57f2ef1762c9f9d1d0f627e70d2f5bdf321e58e9
matt468779 <mengistusima@gmail.com>
2022-01-04 17:00:36 +0300

    modified transaction table response

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     # url(r'^users/$', views.all_users),
-    # url(r'^user/(?P<pk>[a-zA-Z0-9]+)/$', views.user_detail),
+    url(r'^user/(?P<pk>[a-zA-Z0-9]+)/$', views.user_detail),
     # url(r'^transactions/$', views.all_transactions),
     # url(r'^transaction/(?P<pk>[0-9]+)', views.transaction_detail),
     # url(r'^balance/$', views.balance),
